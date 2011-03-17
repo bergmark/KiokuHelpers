0613dc4dcead4e67e47e7a84e7efac6ae9c3242b
Adam Bergmark <adam@edea.se>
2011-03-17 10:31:42 +0100

    fixed reqs again.

--- nodewebhelpers.js
+++ nodewebhelpers.js
@@ -1,4 +1,6 @@
 require('task-joose-nodejs');
+require('Data/UUID');
+require('KiokuJS/Feature/Class/OwnID');
 require('KiokuJS/Feature/Class/OwnUUID');
 require('./CactusJuice/CactusJuice');
 require('./lib/Model');
