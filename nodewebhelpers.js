36c839c631c905b315e3f772af6432dc1528f468
Adam Bergmark <adam@edea.se>
2011-03-21 13:09:33 +0100

    Removed git modules.

--- nodewebhelpers.js
+++ nodewebhelpers.js
@@ -2,6 +2,6 @@ require('task-joose-nodejs');
 require('Data/UUID');
 require('KiokuJS/Feature/Class/OwnID');
 require('KiokuJS/Feature/Class/OwnUUID');
-require('./CactusJuice/CactusJuice');
+require('CactusJuice/CactusJuice');
 require('./lib/Model');
 require('./lib/Repository');
